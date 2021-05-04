a12f2a3631b7402d2c37560a874cf809fad16ec6
imshilpa <shilpageet@gmail.com>
2021-05-04 15:29:39 +0530

    box is added

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var box6;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,6 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    box6 = new Box(300,200,70,70);
 
 }
 
@@ -59,4 +61,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    box6.display();
 }
